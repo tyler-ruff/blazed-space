e4d59fda556e49c21a5e8340c5f2ff07be608176
Tyler Ruff <hello@blazed.space>
2022-12-15 14:21:00 -0500

    Update sib-service.ts

--- src/app/shared/sib-service.ts
+++ src/app/shared/sib-service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root',
