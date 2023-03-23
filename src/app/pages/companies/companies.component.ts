9a95d1a9b31c4461be53983d96fcf684257241cd
Tyler Ruff <hello@blazed.space>
2023-03-22 21:07:20 -0400

    BLZ

--- src/app/pages/companies/companies.component.ts
+++ src/app/pages/companies/companies.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { AppService } from 'src/app/shared/app.service';
 
+import { page } from './companies.page'; 
 import { Company } from 'src/app/models/company.model';
 
-import { page } from './companies.page'; 
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-companies',
@@ -14,56 +16,12 @@ import { page } from './companies.page';
 export class CompaniesComponent implements OnInit {
 
   public page = page;
+  public companies$: Observable<Company[]>;
 
-  companies: Company[] = [
-    {
-      name: "Blazed Labs LLC",
-      description: "We turn Dreams into Reality!",
-      industry: "Software Development",
-      location: "New Jersey, USA",
-      email: "hello@blazed.space",
-      url: "https://blazed.company/",
-      phone: "+18557882348",
-      logo: "https://blazed.sirv.com/logo/Beaker-Dark.png",
-      entityId: "0450452884"
-    },
-    {
-      name: "Concrete Games LLC",
-      description: "A solid game development studio.",
-      industry: "Game Development",
-      location: "New Jersey, USA",
-      email: "hello@blazed.games",
-      url: "https://blazed.games/",
-      phone: "+18882008103",
-      logo: "https://blazed.sirv.com/blazed.games/Concrete-Games-White.png",
-      entityId: "0450847331"
-    },
-    {
-      name: "RTM Transportation LLC",
-      description: "Moving America forward.",
-      industry: "Logistics",
-      location: "New Jersey, USA",
-      email: "hello@rtmtransit.com",
-      url: "https://rtmtransit.com/",
-      phone: "+16092149678",
-      logo: "https://blazed.sirv.com/RTM/logo/RTM-Transit-Square.png",
-      entityId: "0450194330"
-    },
-    {
-      name: "Ruff Management Inc.",
-      description: "Hard working people choose Ruff.",
-      industry: "Asset Management",
-      location: "New Jersey, USA",
-      email: "hello@ruff-manage.com",
-      url: "https://ruff-manage.com/",
-      phone: "+18338870061",
-      logo: "https://blazed.sirv.com/ruff-manage.com/ruff-lion.png",
-      entityId: "0450842898"
-    }
-  ] as Company[];
-
-  constructor(private appService: AppService) {
+  constructor(private appService: AppService, firestore: Firestore) {
     appService.setPage(page);
+    const col = collection(firestore, 'companies');
+    this.companies$ = collectionData(col) as Observable<Company[]>;
   }
 
   ngOnInit(): void {
